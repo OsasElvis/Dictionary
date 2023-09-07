38a61ba31c70bf1c03cdc0d2819fb3fde75edefa
OsasElvis <osarumwenseelvis@outlook.com>
2023-09-07 11:06:19 +0100

    my first commit

--- script.js
+++ script.js
@@ -21,14 +21,19 @@ btn.addEventListener("click", () => {
         <p>${data[0].meanings[0].partOfSpeech}</p>
         <p>/${data[0].phonetic}/</p>
       </div>
+      <div class="contain">
       <p class="word-meaning">
-       ${data[0].meanings[0].definitions[0].definition}
+       ${data[0].meanings[0].definitions.map(data => `<P class="word-meaning">${data.definition}</p>`)}
       </p>
       <p class="word-example">
        ${data[0].meanings[0].definitions[0].example || ""};
-      </p>`;
+      </p>
+      </div>`;
       sound.setAttribute("src", `${data[0].phonetics[1].audio}`);
-    });
+    })
+    .catch(() => {
+      result.innerHTML = `<h3 class="error"> Word not found </h3>`
+    })
 });
 function playSound() {
   sound.play();
